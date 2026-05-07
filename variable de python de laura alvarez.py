5c7b99c54757d23c38970c9b6def4a64d2c8d05c
lauraalvarezcastillo10-collab <lauraalvarezcastillo.10@gmail.com>
2026-05-07 10:08:00 -0500

    Primer ejercicio con python
    Empezando a trabajar con Python, aprendiendo sobre comentarios, variables, función print, función input, función type y concatenación.

---
+++ variable de python de laura alvarez.py
@@ -0,0 +1,33 @@
+#Esto es un comentario de una sola linea
+"""Esto es un comentario de
+varias lineas"""
+
+#Inicializando variables
+nombre="Laura ALVAREZ"
+edad=14
+estado=True
+nota=5.0
+
+print(edad)
+print(estado)
+print(nota)
+#Mostrar el contenido de las variables print()
+print(nombre)
+
+#Que tipo de dato contiene cada variable.
+print(type(nombre))
+print/(type(edad))
+print(type(estado))
+print(type(nota))
+
+#vamos a utilizar la funcion input para recoger datos por medio del teclado.
+nombre=input("¿Como te llamas?  ")
+edad=input("¿Que edad tienes?  ")
+estado=input("¿En que estado te encuentras?  ")
+nota=input("¿Cual es tu nota?  ")
+
+#Para utilizar que guaradamos en las variables anteriores
+print("Hola,",nombre,"un gusto conocerte")
+print("tu edad es:",edad)
+print("Tu estado es:",estado)
+print("TU nota es:",nota)
